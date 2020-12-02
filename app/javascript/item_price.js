3934880dfda77f96aaf25a7a38b2f166cd4af834
masaki524 <d.g.pda0524@gmail.com>
2020-12-02 15:14:38 +0900

    javascriptを用いて販売手数料・販売利益の表示機能を実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,14 @@
 window.addEventListener('load', function(){
-  const priceInput = document.getElementById("item-price")
+  const priceInput = document.getElementById("item-price");
+
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value
-    console.log(inputValue)
+  
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+
+    const Profit = document.getElementById("profit");
+      const taxprice = inputValue * 0.1
+    profit.innerHTML = (Math.floor(inputValue - taxprice));
   })
-})
+});
