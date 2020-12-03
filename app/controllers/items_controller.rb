667d99bdaca13df029cedd0b16bcba9eaf0774e5
masaki524 <d.g.pda0524@gmail.com>
2020-12-03 23:58:20 +0900

    updateの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,17 @@ class ItemsController < ApplicationController
     @item =Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+
+
+  end
+
 
   private
 
