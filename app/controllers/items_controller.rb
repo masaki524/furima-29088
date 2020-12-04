08a33a7a14ec90394e32bee9bfcf747c3feb4b31
masaki524 <d.g.pda0524@gmail.com>
2020-12-04 14:55:54 +0900

    viewの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,8 +38,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = item.find(params[:id])
+    item = Item.find(params[:id])
     item.destroy
+    redirect_to root_path
   end
 
 
