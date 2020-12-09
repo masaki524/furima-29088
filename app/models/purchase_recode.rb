8ea9a997290957c5d64a6322fd25665f2abd4c6d
masaki524 <d.g.pda0524@gmail.com>
2020-12-10 01:04:05 +0900

    データの保存先を設定

--- app/models/purchase_recode.rb
+++ app/models/purchase_recode.rb
@@ -1,13 +1,26 @@
 class PurchaseRecode < ApplicationRecord
   include ActiveModel::Model
-  attr_accessor :post_num, :prefecture_id, :city, :banch, :phone_num, :building_name
+  attr_accessor :post_num, :prefecture_id, :city, :banch, :phone_num, :building_name, :token
 
 
-  validates :post_num, presence: true , format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
-  validates :prefecture_id, presence: true ,numericality: { other_than: 0}
-  validates :banch, presence: true , 
-  validates :city, presence: true , 
-  validates :phone_num, presence: true , 
-  validates :building_name, format: {with: /\A0[5789]0\d{4}\d{4}\z/}
+  with_options presence: true do
+    validates :post_num , format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id ,numericality: { other_than: 0}
+    validates :banch 
+    validates :city  
+    validates :phone_num , format: {with: /\A0[5789]0\d{4}\d{4}\z/}
+    validates :token
+  end
 
+  validates :building_name
+
+  belongs_to :user
+  belongs_to :item
+
+  def save
+    # 配送先住所の情報を保存
+    Shipping_address.create(post_num: post_num, prefecture_id: prefecture_id, city: city, banch: banch, phone_num: phone_num, building_name: building_name, user_id: user.id)
+    # クレジット情報の保存
+    Purchase_recode.create(token: token, user_id: user.id)
+  end
 end
