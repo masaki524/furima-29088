95fa958fa9fa41935cbcd25f459570d378346a9f
masaki524 <d.g.pda0524@gmail.com>
2020-12-10 00:49:00 +0900

    配送先のバリデーションの設定

--- app/models/purchase_recode.rb
+++ app/models/purchase_recode.rb
@@ -1,7 +1,13 @@
 class PurchaseRecode < ApplicationRecord
-  belongs_to :user
-  belongs_to :item
+  include ActiveModel::Model
+  attr_accessor :post_num, :prefecture_id, :city, :banch, :phone_num, :building_name
 
-  has_one :shipping_address
+
+  validates :post_num, presence: true , format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+  validates :prefecture_id, presence: true ,numericality: { other_than: 0}
+  validates :banch, presence: true , 
+  validates :city, presence: true , 
+  validates :phone_num, presence: true , 
+  validates :building_name, format: {with: /\A0[5789]0\d{4}\d{4}\z/}
 
 end
