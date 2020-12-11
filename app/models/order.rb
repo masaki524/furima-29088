0911d9cca205b9ec07091282949846e52e4001f8
masaki524 <d.g.pda0524@gmail.com>
2020-12-11 23:16:48 +0900

    formオブジェクトのインスタンス修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
 
   belongs_to :user
   belongs_to :item
+  
   has_one :shipping_address
 
 end
--- app/models/purchase_recode.rb
+++ app/models/purchase_recode.rb
@@ -5,13 +5,17 @@ class PurchaseRecode < ApplicationRecord
 
   with_options presence: true do
     validates :post_num , format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
-    validates :prefecture_id ,numericality: { other_than: 0}
     validates :banch 
     validates :city  
     validates :phone_num , format: {with: /\A0[5789]0\d{4}\d{4}\z/}
   end
-
   validates :building_name
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  validates :prefecture_id ,numericality: { other_than: 1}
+
 
   def save
     # 購入者情報を保存
