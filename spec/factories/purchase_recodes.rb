b7f0b17b56e6c7d60d78fe06086918bbcfa6d507
masaki524 <d.g.pda0524@gmail.com>
2020-12-13 11:27:13 +0900

    テストコードの記入

--- spec/factories/purchase_recodes.rb
+++ spec/factories/purchase_recodes.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :purchase_recode do
     
+    post_num {"111-1111"}
+    prefecture_id {2}
+    city {"tokyo"}
+    banch {"1-11-1"}
+    phone_num {"09012341234"}
+    building_name {"test"}
   end
 end
--- spec/models/purchase_recode_spec.rb
+++ spec/models/purchase_recode_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseRecode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入情報の保存' do
+    before do
+      @purchase_recode = FactoryBot.build(:purchase_recode)
+    end
+
+    it '全ての値が正しく入力されていれば保存できる' do
+      expect(@purchase_recode).to be_valid
+    end
+
+    it '建物名がなくても保存できる' do
+      @purchase_recode.building_name = nil
+      @purchase_recode.valid?
+      expect(@purchase_recode).to be_valid
+    end
+
+
+    it '郵便番号が入力されていないと保存できない' do
+      @purchase_recode.post_num = nil
+      @purchase_recode.valid?
+      expect(@purchase_recode.errors.full_messages).to include("Post num can't be blank")
+    end
+
+    it '郵便番号はハイフンがなければ保存できない' do
+      @purchase_recode.post_num =  "1111111"
+      @purchase_recode.valid?
+      expect(@purchase_recode.errors.full_messages).to include("Post num is invalid")
+    end
+
+    it '都道府県が入力されていないと保存できない' do
+      @purchase_recode.prefecture_id = 1
+      @purchase_recode.valid?
+      expect(@purchase_recode.errors.full_messages).to include("Prefecture must be other than 1")
+    end
+
+    it '市区町村が入力されていないと保存できない' do
+      @purchase_recode.city = nil
+      @purchase_recode.valid?
+      expect(@purchase_recode.errors.full_messages).to include("City can't be blank")
+
+    end
+
+    it '番地が入力されていないと保存できない' do
+      @purchase_recode.banch = nil
+      @purchase_recode.valid?
+      expect(@purchase_recode.errors.full_messages).to include("Banch can't be blank")
+    end
+
+    it '電話番号が入力されていないと保存できない' do
+      @purchase_recode.phone_num = nil
+      @purchase_recode.valid?
+      expect(@purchase_recode.errors.full_messages).to include("Phone num can't be blank")
+    end
+
+    it '電話番号はハイフンがあると保存できない' do
+      @purchase_recode.phone_num = "090-1234-1234" 
+      @purchase_recode.valid?
+      expect(@purchase_recode.errors.full_messages).to include("Phone num is invalid")
+    end
+  end
 end
